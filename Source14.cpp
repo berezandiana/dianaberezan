4f5643ec0000589661e1a78efdbd5be9ec5003e4
berezandiana <berezandiana24@gmail.com>
2022-12-05 18:12:10 +0200

    Update Source14.cpp
    good

--- Source14.cpp
+++ Source14.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 int main()
 {
-    int arr[][3] = { {1, 2, 3},
+    int arr[][3] = { {45, 2, 3},
                     {4, 5, 6},
-                    {6,-5, 4},
-                    {3, 2, 1} };
+                    {6,-5, 46},
+                    {3, 35, 1} };
     int min = arr[0][0];
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 3; j++)
